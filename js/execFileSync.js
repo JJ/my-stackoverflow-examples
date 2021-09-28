60a860d4d9ca4c045024f7c55a550904ad01146d
JJ Merelo <jjmerelo@gmail.com>
2021-09-28 10:43:19 +0200

    New version that seems to work

--- js/execFileSync.js
+++ js/execFileSync.js
@@ -4,5 +4,5 @@ const path = require("path");
 
 const targetArchive = "/tmp/example.zip";
 const sourceDir = path.join(__dirname, "node_modules", "sprintf-js", "dist");
-const args = ["-r", targetArchive, "*"];
-child_process.execFileSync("zip", args, { cwd: sourceDir, shell: true });
+const args = ["-r", "/tmp/*"];
+child_process.execFileSync("ls", args, { cwd: sourceDir, shell: true });
